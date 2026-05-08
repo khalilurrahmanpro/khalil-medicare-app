35127559ba69aa7cf9c2b5f359524d26984e1a39
khalilurrahmanpro <mdkhlilurrahmanpro@gmail.com>
2026-05-09 03:33:37 +0600

    database code change something

--- backend/settings.py
+++ backend/settings.py
@@ -76,7 +76,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 # Password validation
