e1ca10e899f8af0d21307a22f864381b483058c0
khalilurrahmanpro <mdkhlilurrahmanpro@gmail.com>
2026-05-16 13:52:26 +0600

    change serialize

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Order, OrderItem, Medicine
+from .models import Order, OrderItem, Medicine, Prescription
+from django.contrib.auth.models import User
 
 class OrderItemSerializer(serializers.ModelSerializer):
     medicine_name = serializers.ReadOnlyField(source='medicine.name')
@@ -8,15 +9,31 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OrderItem
-        fields = ['id', 'medicine_name', 'unit_price', 'unit_type', 'quantity', 'discount', 'subtotal']
+        fields = ['id', 'medicine_name', 'unit_price', 'unit_type', 'quantity', 'discount']
 
 class OrderSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='user.username')
+    phone = serializers.ReadOnlyField(source='user.first_name') 
     items = OrderItemSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         fields = [
-            'id', 'username', 'address', 'phone', 'total_price', 
-            'status', 'payment_method', 'created_at', 'items'
+            'id', 'username', 'items', 'total_price', 'address', 
+            'phone', 'status', 'payment_method', 'created_at'
         ]
+
+class MedicineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medicine
+        fields = '__all__'
+
+class PrescriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prescription
+        fields = '__all__'
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'is_staff', 'is_superuser']
