7b53d5e4f35e60e4c3c870ae090de59e3b874ef5
khalilurrahmanpro <mdkhlilurrahmanpro@gmail.com>
2026-06-21 22:03:54 +0600

    Fixed profile image error

--- core/models.py
+++ core/models.py
@@ -44,6 +44,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='profiles/', blank=True, null=True) 
     def __str__(self):
         return self.user.username
     
