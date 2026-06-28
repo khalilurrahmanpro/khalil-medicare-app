b59254957bce786e25695e4e26867dc72883a9ed
khalilurrahmanpro <mdkhlilurrahmanpro@gmail.com>
2026-06-28 09:05:58 +0600

    added admin register

--- core/admin.py
+++ core/admin.py
@@ -1,20 +1,15 @@
 from django.contrib import admin
 from .models import Medicine, Prescription, Profile, Order, Category
 
-# সাধারণ রেজিস্ট্রেশন
-admin.site.register(Medicine)
 admin.site.register(Prescription)
 admin.site.register(Order)
 
-# ক্যাটাগরি রেজিস্ট্রেশন
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
 
-# প্রোফাইল রেজিস্ট্রেশন (সংশোধিত)
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    # এখানে phone এবং address আপনার মডেলের নামের সাথে মিল থাকতে হবে
     list_display = ['user', 'phone', 'address',]
 
 @admin.register(Medicine)
